8e297bb5921e0da7f9955710cf25491419d53776
Maria Belan-Crawford <44656740+mViolet@users.noreply.github.com>
2021-05-23 15:27:21 -0500

    update script.js - string kata

--- script.js
+++ script.js
@@ -1,31 +1,63 @@
-//2021 05.22
+//2021 05.23
 
-//salesman's travel
-//params: r = address list, zipcode to search
-//return: zipcode:street and town,street and town,.../house number,house number,...
-function travel(r, zip) {
-    console.log(`\n\nr: [${r}]\nzip: [${zip}]\n\n`)
-    const list = r.split(',')
-    const adds = []
-    const nums = []
-    let res = '/'
-    list.forEach((add, i) => {
-        if (add.includes(zip)) {
-            adds.push(list[i].trim().split(' ').slice(1, -2).join(' '))
-            nums.push(list[i].trim().split(' ')[0])
+// Count characters in your string
+function count(string) {
+    let chars = {}
+
+    string.split('').forEach(c => {
+        if (!chars[c]) {
+            chars[c] = 1
+        } else {
+            chars[c]++
         }
     })
-    res = adds.join(',') + '/' + nums.join(',')
-    // console.log(adds.join(','), nums.join(','))
-    console.log(zip + ':' + res)
-      console.log(nums)
-    return zip + ':' + res
+    return chars
 }
 
-let add = "123 Main Street St.Louisville OH 43071, 432 Main Long Road St.Louisville OH 43071, 786 High Street Pollocksville NY 56432, 54 Holy Grail Street Niagara Town ZP 32908, 3200 Main Rd.Bern AE 56210, 1 Gordon St.Atlanta RE 13000, 10 Pussy Cat Rd.Chicago EX 34342, 10 Gordon St.Atlanta RE 13000, 58 Gordon Road Atlanta RE 13000, 22 Tokyo Av.Tedmondville SW 43098, 674 Paris bd.Abbeville AA 45521, 10 Surta Alley Goodtown GG 30654, 45 Holy Grail Al.Niagara Town ZP 32908, 320 Main Al.Bern AE 56210, 14 Gordon Park Atlanta RE 13000, 100 Pussy Cat Rd.Chicago EX 34342, 2 Gordon St.Atlanta RE 13000, 5 Gordon Road Atlanta RE 13000, 2200 Tokyo Av.Tedmondville SW 43098, 67 Paris St.Abbeville AA 45521, 11 Surta Avenue Goodtown GG 30654, 45 Holy Grail Al.Niagara Town ZP 32918, 320 Main Al.Bern AE 56215, 14 Gordon Park Atlanta RE 13200, 100 Pussy Cat Rd.Chicago EX 34345, 2 Gordon St.Atlanta RE 13222, 5 Gordon Road Atlanta RE 13001, 2200 Tokyo Av.Tedmondville SW 43198, 67 Paris St.Abbeville AA 45522, 11 Surta Avenue Goodville GG 30655, 2222 Tokyo Av.Tedmondville SW 43198, 670 Paris St.Abbeville AA 45522, 114 Surta Avenue Goodville GG 30655, 2 Holy Grail Street Niagara Town ZP 32908, 3 Main Rd.Bern AE 56210, 77 Gordon St.Atlanta RE 13000"
+// //Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins
+// var list1 = [
+//     { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+//     { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
+//     { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
+//     { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
+// ]
+
+// function findAdmin(list, lang) {
+//     return list.filter(el => el.language == lang && el.githubAdmin == 'yes')
+// }
 
 
-console.log(travel(add, 'AA 45522'))
+//2021 05.22
+
+// //salesman's travel
+// //params: r = address list, zipcode to search
+// //return: zipcode:street and town,street and town,.../house number,house number,...
+// //I have created a monster
+// function travel(r, zip) {
+//     console.log(`\n\nr: [${r}]\nzip: [${zip}]\n\n`)
+//     const list = r.split(',')
+//     const adds = []
+//     const nums = []
+//     let res = '/'
+//     if (zip == [] || zip.length !== 8) return zip + ':' + res
+//     list.forEach((add, i) => {
+//         if (add.includes(zip)) {
+//             adds.push(list[i].trim().split(' ').slice(1, -2).join(' '))
+//             nums.push(list[i].trim().split(' ')[0])
+//         }
+//     })
+//     res = adds.join(',') + '/' + nums.join(',')
+//     // console.log(adds.join(','), nums.join(','))
+//     console.log(zip + ':' + res)
+//     //       console.log(nums)
+//     return zip + ':' + res
+// }
+
+// let add = "123 Main Street St.Louisville OH 43071, 432 Main Long Road St.Louisville OH 43071, 786 High Street Pollocksville NY 56432, 54 Holy Grail Street Niagara Town ZP 32908, 3200 Main Rd.Bern AE 56210, 1 Gordon St.Atlanta RE 13000, 10 Pussy Cat Rd.Chicago EX 34342, 10 Gordon St.Atlanta RE 13000, 58 Gordon Road Atlanta RE 13000, 22 Tokyo Av.Tedmondville SW 43098, 674 Paris bd.Abbeville AA 45521, 10 Surta Alley Goodtown GG 30654, 45 Holy Grail Al.Niagara Town ZP 32908, 320 Main Al.Bern AE 56210, 14 Gordon Park Atlanta RE 13000, 100 Pussy Cat Rd.Chicago EX 34342, 2 Gordon St.Atlanta RE 13000, 5 Gordon Road Atlanta RE 13000, 2200 Tokyo Av.Tedmondville SW 43098, 67 Paris St.Abbeville AA 45521, 11 Surta Avenue Goodtown GG 30654, 45 Holy Grail Al.Niagara Town ZP 32918, 320 Main Al.Bern AE 56215, 14 Gordon Park Atlanta RE 13200, 100 Pussy Cat Rd.Chicago EX 34345, 2 Gordon St.Atlanta RE 13222, 5 Gordon Road Atlanta RE 13001, 2200 Tokyo Av.Tedmondville SW 43198, 67 Paris St.Abbeville AA 45522, 11 Surta Avenue Goodville GG 30655, 2222 Tokyo Av.Tedmondville SW 43198, 670 Paris St.Abbeville AA 45522, 114 Surta Avenue Goodville GG 30655, 2 Holy Grail Street Niagara Town ZP 32908, 3 Main Rd.Bern AE 56210, 77 Gordon St.Atlanta RE 13000"
+
+
+// console.log(travel(add, 'AA 45522'))
+
 //2021 05.21
 
 // //Alphabet Symmetry
