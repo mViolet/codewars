c6750b8467ad338c24cd2fca59fb147e7416a83b
Maria Belan-Crawford <44656740+mViolet@users.noreply.github.com>
2021-08-18 15:52:49 -0500

    update script.js

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
+//2021 08.18
+
+//simple pig latin
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word.Leave punctuation marks untouched.
+
+function pigIt(str){
+    return str.replace(/\w+/gi, w => w.slice(1) + w[0] + 'ay')
+}
+
+console.log(pigIt('Pig latin is cool')) // igPay atinlay siay oolcay
+console.log(pigIt('Hello world!'))     // elloHay orldway !
+
+
 //2021 08.17
+
+//from frontend masters algos course
+
+// Task: Transform a simple sorting algorithm into a unique sort.  It should not return any duplicate values in the sorted Array.
+
+//input: [1,5,2,1] output: [1,2,5]
+//input: [4,2,2,3,2,2,2] output: [2,3,4]
+
+// Set solution
+const uniqSort = function(arr){
+    const breadcrumbs = {}
+
+    return [... new Set(arr.sort((a, b) => a - b))]
+}
+
+//another way
+// const uniqSort = function (arr) {
+//     return arr.sort((a, b) => a - b).filter((el,i,a) => el !== a[i+1])
+// }
+
+console.log(uniqSort([4,2,2,3,2,2,2]), [2,3,4])
 
 // Given a magazine of words and a ransom note, determine if it’s possible to “cut out” and create the ransom note from the magazine words.
 
-const magazine =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+// const magazine =
+//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 
-function ransomNote(str){
-    return str.split(' ').every(word => magazine.includes(word))
-}
+// function ransomNote(str){
+//     return str.split(' ').every(word => magazine.includes(word))
+// }
 
-console.log(ransomNote("sit ad est sint", magazine), true)
-console.log(ransomNote("sit ad est love", magazine), false)
-console.log(ransomNote("sit ad est sint in in", magazine), true)
-console.log(ransomNote("sit ad est sint in in in in", magazine), false)
+// console.log(ransomNote("sit ad est sint", magazine), true)
+// console.log(ransomNote("sit ad est love", magazine), false)
+// console.log(ransomNote("sit ad est sint in in", magazine), true)
+// console.log(ransomNote("sit ad est sint in in in in", magazine), false)
 
 //2021 08.16
 
